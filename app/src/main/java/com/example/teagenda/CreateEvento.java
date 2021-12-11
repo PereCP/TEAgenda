c68060078b26f6cb2d7016482713c9bbb2d75f71
Pere Conte <pere.conte1@gmail.com>
2021-12-11 18:04:41 +0100

    Started CreateEvento

---
+++ app/src/main/java/com/example/teagenda/CreateEvento.java
@@ -0,0 +1,13 @@
+package com.example.teagenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateEvento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_evento);
+    }
+}
