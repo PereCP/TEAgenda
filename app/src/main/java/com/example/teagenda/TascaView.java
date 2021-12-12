3210554f71cfefa03d357dbca4d3a4a0e5a4ae32
alvaro <alvaro.rodriguez.rubio@estudiantat.upc.edu>
2021-12-12 11:02:36 +0100

    icono

--- app/src/main/java/com/example/teagenda/TascaView.java
+++ app/src/main/java/com/example/teagenda/TascaView.java
@@ -3,6 +3,7 @@ package com.example.teagenda;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,11 +17,12 @@ import java.sql.Time;
 import java.util.Date;
 
 public class TascaView extends AppCompatActivity {
-    Button buttonExit;
+
     private Evento event;
     private TextView NameText;
     private TextView date;
     private TextView desc;
+    private View barra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,20 +32,20 @@ public class TascaView extends AppCompatActivity {
 
         event = DomainController.getInstance().getSelectedEvento();
         setContentView(R.layout.activity_tasca_view);
-        buttonExit = (Button)findViewById(R.id.buttonExit);
+        barra = (View)findViewById(R.id.barra);
         NameText = (TextView)findViewById(R.id.EventName);
         date = (TextView)findViewById(R.id.Date);
         desc = (TextView)findViewById(R.id.EventDesc);
         NameText.setText(event.getTitle());
         date.setText(event.getDate().toString());
-        desc.setText(event.getDate().toString());
-        buttonExit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent tornar = new Intent(TascaView.this, MainActivity.class);
-                startActivity(tornar);
-            }
-        });
+        desc.setText(event.getDescription());
+        int color = event.getColor();
+
+
+        barra.setBackgroundColor(color);
+
+        //if(event.getColor());
+
     }
 
     @Override
